1272ac3c70e18372e0b4f20fc64de23d1911813b
Suna <75311863+sunasaribas@users.noreply.github.com>
2021-11-22 22:48:46 +0300

    Add project files.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cumle = "bu gün hava çok güzel....arkadaşlar ile kavak olan bir yere gittik. tabi gitmeden ütü yaptım. orada ata bindim....";
+
+            string[] cumles = cumle.Split(' ');
+            bool isPalindrome = false;
+            foreach (var item in cumles)
+            {
+                int bas = 0, son = 0;
+                son = item.Length - 1;
+                int l = item.Length / 2;
+                for (int i = 0; i < l; i++)
+                {
+                    char ilkK, sonK;
+
+                    ilkK = item[bas++];
+                    sonK = item[son--];
+
+                    if (ilkK == sonK)
+                    {
+                        isPalindrome = true;
+                    }
+                    else
+                    {
+                        isPalindrome = false;
+                        break; // döngüdenden çık...
+                    }
+                }
+                if (isPalindrome)
+                {
+                    Console.WriteLine($" {item} palindrome");
+                }
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
